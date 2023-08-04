9cc9ac4ee307058cc89dbbf5c17ffe8c6566c59a
56radius <cussmeritmohammed@gmail.com>
2023-08-04 23:13:40 +0100

    Sixth Commit

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,9 +8,21 @@ import {
   ImageBackground,
   ScrollView,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function SignUpScreen({ navigation }) {
+  const [password, setPassword] = useState("");
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const handleDateChange = (event, selectedDate) => {
+    setShowDatePicker(false);
+    if (selectedDate) {
+      setBirthDate(selectedDate);
+    }
+  };
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -29,12 +41,107 @@ export default function SignUpScreen({ navigation }) {
           </Text>
 
           {/* paragraph */}
-          <View>
-            <Text> Please enter your qualification details </Text>
+          <View style={{ paddingVertical: 10 }}>
+            <Text style={{ color: "#708090" }}>
+              Please enter your qualification details
+            </Text>
           </View>
         </View>
 
         {/* Form */}
+        <View style={styles.form}>
+          {/* Full Name input */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Full Name"
+              keyboardType="default"
+              autoCapitalize="words"
+            />
+          </View>
+
+          {/* Birthday Date */}
+          <View>
+            <TouchableOpacity
+              style={styles.inputContainer}
+              onPress={() => setShowDatePicker(true)}
+            >
+              <TextInput
+                style={styles.input}
+                placeholder="Select Birth Date"
+                value={birthDate.toDateString()}
+                editable={false}
+              />
+            </TouchableOpacity>
+
+            {showDatePicker && (
+              <DateTimePicker
+                value={birthDate}
+                mode="date"
+                display="spinner"
+                onChange={handleDateChange}
+              />
+            )}
+          </View>
+
+          {/* gender */}
+
+          {/* Email */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Email address"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          </View>
+
+          {/* Password */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              placeholder="password"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+          </View>
+
+          {/* Confirm password */}
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              placeholder="password"
+              autoCapitalize="none"
+            />
+          </View>
+
+          {/* Submit */}
+          <View style={styles.Submit}>
+            <TouchableOpacity
+              style={{
+                borderWidth: 2,
+                borderColor: "green",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingVertical: 10,
+                borderRadius: 20,
+                backgroundColor: "green",
+              }}
+            >
+              <Text style={{ fontWeight: "bold", color: "#fff" }}>SUBMIT</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Forgot Password and Sign Up */}
+          <View style={styles.rowContainer}>
+            <Text> Already Have an account? </Text>
+            <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+              <Text style={styles.signUpButton}> Login </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
         <StatusBar style="auto" />
       </View>
     </ScrollView>
@@ -47,14 +154,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1,
     margin: 30,
+    paddingVertical: 20,
   },
   normalText: {
     fontSize: 30,
     color: "green",
+    fontWeight: "bold",
   },
 
   highlightedText: {
     fontSize: 30,
     color: "blue",
+    fontWeight: "bold",
+  },
+
+  form: {
+    padding: 20,
+    width: "100%",
+  },
+
+  inputContainer: {
+    marginBottom: 15,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+    borderColor: "blue",
+  },
+  input: {
+    fontSize: 16,
   },
 });
