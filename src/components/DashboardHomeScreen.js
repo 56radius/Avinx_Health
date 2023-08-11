5c681566505bcbe0a224d7e94e20744a8e7a6466
56radius <cussmeritmohammed@gmail.com>
2023-08-11 15:29:30 +0100

    Modified Dashboard Home Screen

--- src/components/DashboardHomeScreen.js
+++ src/components/DashboardHomeScreen.js
@@ -9,6 +9,14 @@ import {
   FlatList,
 } from "react-native";
 import * as React from "react";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
 import { Ionicons } from "@expo/vector-icons";
 import { getAuth, signOut } from "firebase/auth";
 
@@ -22,100 +30,114 @@ export default function DashboardHomeScreen({ navigation }) {
 
   // Sample card data
   const cardData = [
-    { id: "1", title: "Hello", content: "Content for Card 1" },
-    { id: "2", title: "Card 2", content: "Content for Card 2" },
-    { id: "3", title: "Card 3", content: "Content for Card 3" },
+    { id: "1", title: "Feedback", content: "Content for Card 1" },
+    { id: "2", title: "Mind Health Trends", content: "Content for Card 2" },
+    { id: "3", title: "Community", content: "Content for Card 3" },
+    { id: "4", title: "Card 4", content: "Jesus is good" },
   ];
 
-  const renderItem = ({ item }) => (
+  const renderItem = ({ item, index }) => (
     <View style={styles.card}>
       <Text style={styles.cardTitle}>{item.title}</Text>
       <Text>{item.content}</Text>
+      {/* Add any other components you want for each card */}
+      <TouchableOpacity style={styles.cardButton}>
+        <Text style={styles.cardButtonText}>Learn More</Text>
+      </TouchableOpacity>
     </View>
   );
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {/* Header and greetings */}
-      <View style={styles.header}>
-        <View style={styles.userInfo}>
-          <Image
-            style={{ width: 30, height: 30 }}
-            source={require("../.././assets/woman.jpg")}
-          />
-          <Text style={styles.greetingText}>
-            Good Evening,
-            {"\n"}
-            <Text style={styles.userName}>Anu</Text>
-          </Text>
-        </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {/* Header and greetings */}
+        <View style={styles.header}>
+          <View style={styles.userInfo}>
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={require("../.././assets/woman.jpg")}
+            />
+            <Text style={styles.greetingText}>
+              Good Evening,
+              {"\n"}
+              <Text style={styles.userName}>Anu</Text>
+            </Text>
+          </View>
 
-        {/* Notification Icons */}
-        <View
-          style={{
-            borderWidth: 1,
-            paddingHorizontal: 4,
-            paddingVertical: 5,
-            backgroundColor: "#D3D3D3",
-            borderColor: "#D3D3D3",
-            borderRadius: 8,
-          }}
-        >
-          <Ionicons name="ios-notifications-outline" size={24} color="black" />
-        </View>
-      </View>
-
-      {/* Card */}
-      <View style={styles.cardHeader}>
-        <View style={styles.textCardHeader}>
-          <Text
+          {/* Notification Icons */}
+          <View
             style={{
-              fontWeight: "bold",
-              fontSize: 20,
+              borderWidth: 1,
+              paddingHorizontal: 4,
               paddingVertical: 5,
-              color: "#fff",
+              backgroundColor: "#D3D3D3",
+              borderColor: "#D3D3D3",
+              borderRadius: 8,
             }}
           >
-            Keep it up!
-          </Text>
-          <Text style={{ fontSize: 12, color: "#fff" }}>
-            You are never alone okay? {"\n"} Always remember that
-          </Text>
+            <Ionicons
+              name="ios-notifications-outline"
+              size={24}
+              color="black"
+            />
+          </View>
         </View>
-        <View style={{ position: "absolute", marginLeft: 210 }}>
-          <Image
-            style={{ width: 130, height: 110 }}
-            source={require("../.././assets/logo.png")}
+
+        {/* Card */}
+        <View style={styles.cardHeader}>
+          <View style={styles.textCardHeader}>
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 20,
+                paddingVertical: 5,
+                color: "#fff",
+              }}
+            >
+              Keep it up!
+            </Text>
+            <Text style={{ fontSize: 12, color: "#fff" }}>
+              You are never alone okay? {"\n"} Always remember that
+            </Text>
+          </View>
+          <View style={{ position: "absolute", marginLeft: 210 }}>
+            <Image
+              style={{ width: 130, height: 110 }}
+              source={require("../.././assets/logo.png")}
+            />
+          </View>
+          <TouchableHighlight onPress={handleSignOut}>
+            <Text>SIGN OUT</Text>
+          </TouchableHighlight>
+        </View>
+
+        {/* line up cards */}
+        <View style={styles.lineCardheader}>
+          <View style={styles.lineCardInfo}>
+            <Text style={styles.lineCardText}> A Better You </Text>
+          </View>
+
+          {/* see all */}
+          <View>
+            <TouchableOpacity>
+              <Text style={{ color: "green" }}> See all </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Slideshow of Cards */}
+        <View style={styles.slideshow}>
+          <FlatList
+            data={cardData}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
           />
         </View>
-        <TouchableHighlight onPress={handleSignOut}>
-          <Text>SIGN OUT</Text>
-        </TouchableHighlight>
-      </View>
 
-      {/* line up cards */}
-      <View style={styles.lineCardheader}>
-        <View style={styles.lineCardInfo}>
-          <Text style={styles.lineCardText}> A Better You </Text>
-        </View>
-
-        {/* see all */}
-        <View>
-          <TouchableOpacity>
-            <Text style={{ color: "green" }}> See all </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      {/* Slideshow of Cards */}
-      <View style={styles.slideshow}>
-        <FlatList
-          data={cardData}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-        />
+        {/* Your Routine */}
+        <View style={styles.routine}></View>
       </View>
     </ScrollView>
   );
@@ -185,11 +207,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 
+  // slideshow card
   slideshow: {
-    width: "100%",
-    height: 150,
+    width: "90%",
+    height: 200,
     marginTop: 0,
-    paddingHorizontal: 20,
+    paddingHorizontal: 3,
   },
 
   card: {
@@ -207,5 +230,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
     marginBottom: 5,
+  },
+
+  chartContainer: {
+    marginTop: 20,
+    alignItems: "center",
   },
 });
