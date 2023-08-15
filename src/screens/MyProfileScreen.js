71202f1187b25fbf72a46807521055de2eb021b3
56radius <cussmeritmohammed@gmail.com>
2023-08-16 00:42:22 +0100

    My Profile screen

--- src/screens/MyProfileScreen.js
+++ src/screens/MyProfileScreen.js
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+//Expo Vector Icons
+import { Ionicons, AntDesign, MaterialIcons } from "@expo/vector-icons";
 
 export default function EditProfileScreen({ navigation }) {
   return (
@@ -25,6 +28,70 @@ export default function EditProfileScreen({ navigation }) {
           <Text style={{ color: "gray" }}> @anulovessleep </Text>
         </Text>
       </View>
+
+      {/* user's info */}
+      <View style={{ flex: 5 }}>
+        {/* Edit Profile */}
+        <TouchableOpacity
+          onPress={() => navigation.navigate("MyProfileScreen")}
+          style={styles.profileItem}
+        >
+          <View style={styles.profileItemLeft}>
+            <AntDesign name="edit" size={24} color="black" />
+            <Text style={styles.profileTitle}>
+              Username {"\n"} <Text> @anulovessleep </Text>
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* */}
+        <TouchableOpacity style={styles.profileItem}>
+          <View style={styles.profileItemLeft}>
+            <Ionicons name="people-outline" size={24} color="black" />
+            <Text style={styles.profileTitle}> Status </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Search */}
+        <TouchableOpacity style={styles.profileItem}>
+          <View style={styles.profileItemLeft}>
+            <Ionicons name="search-outline" size={24} color="black" />
+            <Text style={styles.profileTitle}> Search </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Help and support */}
+        <TouchableOpacity style={styles.profileItem}>
+          <View style={styles.profileItemLeft}>
+            <Ionicons name="ios-help-circle" size={24} color="black" />
+            <Text style={styles.profileTitle}> Support </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Feedback */}
+        <TouchableOpacity style={styles.profileItem}>
+          <View style={styles.profileItemLeft}>
+            <MaterialIcons name="feedback" size={24} color="black" />
+            <Text style={styles.profileTitle}> Feedback </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Data Privacy */}
+        <TouchableOpacity style={styles.profileItem}>
+          <View style={styles.profileItemLeft}>
+            <MaterialIcons name="privacy-tip" size={24} color="black" />
+            <Text style={styles.profileTitle}> Privacy </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Settings */}
+        <TouchableOpacity style={styles.profileItem}>
+          <View style={styles.profileItemLeft}>
+            <AntDesign name="setting" size={24} color="black" />
+            <Text style={styles.profileTitle}> Settings </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -42,5 +109,27 @@ const styles = StyleSheet.create({
     marginTop: 90,
     margin: 30,
     alignSelf: "flex-start",
+  },
+
+  profileItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    paddingBottom: 10,
+  },
+
+  profileItemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 160,
+  },
+
+  profileTitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginLeft: 8,
   },
 });
